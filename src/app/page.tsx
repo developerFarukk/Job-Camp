657eab58c2548218cf1735fa610554f5b6e129f9
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-27 05:39:23 +0600

    DesyUI Setup Complet

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,14 @@ export default function Home() {
   return (
     <div>
       <h2>Jobe Camp Start website</h2>
+      <div className="flex justify-center items-center gap-4 p-4">
+      <button className="btn btn-active">Default</button>
+      <button className="btn btn-active btn-neutral">Neutral</button>
+      <button className="btn btn-active btn-primary">Primary</button>
+      <button className="btn btn-active btn-secondary">Secondary</button>
+      <button className="btn btn-active btn-accent">Accent</button>
+      <button className="btn btn-active btn-ghost">Ghost</button>
+      </div>
     </div>
   );
 }
