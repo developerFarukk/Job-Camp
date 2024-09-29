260ade5da66ae9353ce370d454dafd82f8323768
FARUK2059 <omarfaruk34552@gmail.com>
2024-09-30 02:10:03 +0600

    up jnasbv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,12 +5,11 @@ export default function Home() {
     <div>
       <h2>Jobe Camp Start website</h2>
       <div className="flex justify-center items-center gap-4 p-4">
-      <button className="btn btn-active">Default</button>
-      <button className="btn btn-active btn-neutral">Neutral</button>
+      
       <button className="btn btn-active btn-primary">Primary</button>
       <button className="btn btn-active btn-secondary">Secondary</button>
       <button className="btn btn-active btn-accent">Accent</button>
-      <p>Amer suner bangla ami tumay balobasi</p>
+      
       
       </div>
     </div>
