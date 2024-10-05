044bd58dc7a4263cdadd0664e5693d438c302d9d
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-06 04:55:21 +0600

    section add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,25 @@
 
 export default function Home() {
   return (
-    <div>
-      <h2>Jobe Camp Start website</h2>
-      <div className="flex justify-center items-center gap-4 p-4">
-      
-      <button className="btn btn-active btn-primary">Primary</button>
-      <button className="btn btn-active btn-secondary">Secondary</button>
-      <button className="btn btn-active btn-accent">Accent</button>
-      <h2>Amar suner bangla ami tumay balobasi</h2>
-      <p>kjsdhsdf dskjnjjnnd knanfff </p>
-      <h3>dilka paasyhgcv j </h3>
-      <h2>kjsdj kjhsjkbdf jjdhJs</h2>
+    <section>
       <div>
-        <button className="btn btn-warning"> Subbmit Now </button>
+        <h2>Jobe Camp Start website</h2>
+        <div className="flex justify-center items-center gap-4 p-4">
+
+          <button className="btn btn-active btn-primary">Primary</button>
+          <button className="btn btn-active btn-secondary">Secondary</button>
+          <button className="btn btn-active btn-accent">Accent</button>
+          <h2>Amar suner bangla ami tumay balobasi</h2>
+          <p>kjsdhsdf dskjnjjnnd knanfff </p>
+          <h3>dilka paasyhgcv j </h3>
+          <h2>kjsdj kjhsjkbdf jjdhJs</h2>
+          <div>
+            <button className="btn btn-warning"> Subbmit Now </button>
+          </div>
+
+
+        </div>
       </div>
-      
-      
-      </div>
-    </div>
+    </section>
   );
 }
