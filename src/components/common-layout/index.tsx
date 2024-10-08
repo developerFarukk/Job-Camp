d1cb5248081010b5e618b793bcb0557ab8c01d7e
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-08 14:12:21 +0600

    up 6554

--- src/components/common-layout/index.tsx
+++ src/components/common-layout/index.tsx
@@ -16,6 +16,7 @@ const CommonLayout = ({ children }: { children: React.ReactNode }) => {
             </main>
 
             {/* Footer Components */}
+            <p>This is footer</p>
         </div>
     );
 };
