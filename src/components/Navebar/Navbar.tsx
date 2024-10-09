687baed72aaa92a9834b4d7fa5b21b54408ff652
FARUK2059 <omarfaruk34552@gmail.com>
2024-10-10 02:45:09 +0600

    up setting

--- src/components/Navebar/Navbar.tsx
+++ src/components/Navebar/Navbar.tsx
@@ -2,19 +2,29 @@ import Image from "next/image";
 import Link from "next/link";
 import job from '../../../public/jobcamp.ico'
 import { TfiSettings } from "react-icons/tfi";
+import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
+import { BsClipboard2Check } from "react-icons/bs";
+import { CgProfile } from "react-icons/cg";
+import { MdOutlineDriveFileMove } from "react-icons/md";
+import { SlHeart } from "react-icons/sl";
+import { FaRegMessage } from "react-icons/fa6";
+import { FaBrain } from "react-icons/fa6";
+import React from "react";
 
 
 
 const Navbar = () => {
 
-    const navItems: { title: string; path: string }[] = [
+    const navItems: { title: string; path: string; }[] = [
         {
             title: 'Job Search',
             path: '/',
+
         },
         {
             title: 'Service',
             path: '/service',
+
         },
         {
             title: 'career Advice',
@@ -30,6 +40,39 @@ const Navbar = () => {
         }
     ];
 
+    const settingItems: { title: string; path: string; icon: React.ReactNode }[] = [
+        {
+            title: 'Job Board',
+            path: '/',
+            icon: <BsClipboard2Check className=" p-1 w-6 h-6" />
+        },
+        {
+            title: 'Profile',
+            path: '/service',
+            icon: < CgProfile className=" p-1 w-6 h-6" />
+        },
+        {
+            title: 'Applications',
+            path: '/advice',
+            icon: <MdOutlineDriveFileMove className=" p-1 w-6 h-6" />
+        },
+        {
+            title: 'messages',
+            path: '/notice',
+            icon: <FaRegMessage className=" p-1 w-6 h-6" />
+        },
+        {
+            title: 'Save Jobs',
+            path: '/explore',
+            icon: <SlHeart className=" p-1 w-6 h-6" />
+        },
+        {
+            title: 'Skils Test',
+            path: '/explore',
+            icon: <FaBrain className=" p-1 w-6 h-6" />
+        }
+    ];
+
 
     return (
         <header className="shrink-0 ">
@@ -40,8 +83,43 @@ const Navbar = () => {
                         {/* Menu setting */}
                         <div>
                             <div>
-                                <TfiSettings className="h-10 w-10 p-1 hover:bg-blue-100 rounded-full" />
+
                             </div>
+                            <Sheet>
+                                <SheetTrigger asChild>
+                                    <TfiSettings className="h-10 w-10 p-1 hover:bg-blue-100 rounded-full" />
+                                </SheetTrigger>
+                                <SheetContent side="left">
+                                    <Link className="mr-6 hidden lg:flex" href={"#"}>
+                                        <h3>JOBSCO</h3>
+                                    </Link>
+                                    <div className="grid gap-2 py-6">
+                                        {settingItems?.map((navItem) =>
+                                        (
+                                            <ul key={navItem.path}>
+                                                <div className=" ">
+
+                                                    <Link
+                                                        className='text-gray-800 font-bold lg:text-md  transition hover-underline-animation'
+                                                        href={navItem.path}
+                                                    >
+                                                        <div className="flex gap-2 items-center">
+                                                            <div>{navItem.icon}</div>
+                                                            <div> {navItem.title}</div>
+                                                        </div>
+                                                    </Link>
+                                                </div>
+                                            </ul>
+                                        ))}
+                                        {/* <Moon
+                                            className="cursor-pointer mb-4"
+                                            fill={theme === "dark" ? "light" : "dark"}
+                                            onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+                                        />
+                                        <UserButton afterSignOutUrl="/" /> */}
+                                    </div>
+                                </SheetContent>
+                            </Sheet>
                         </div>
 
                         <Link className="block text-teal-600 hover:bg-blue-50 hover:text-black rounded-full p-2" href="/">
